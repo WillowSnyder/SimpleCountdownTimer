3e2279f76feed457c199563e940e144fe7f39f1e
Wyatt Snyder <Wyatt_Snyder@baylor.edu>
2020-07-30 20:08:16 -0500

    button changes text

--- app/src/main/java/com/gmail/wyatt/james/snyder/simplecountdowntimer/MainActivity.java
+++ app/src/main/java/com/gmail/wyatt/james/snyder/simplecountdowntimer/MainActivity.java
@@ -3,12 +3,21 @@ package com.gmail.wyatt.james.snyder.simplecountdowntimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.testTxt);
+    }
+
+    public void addEvent(View view){
+        text.setText("new event");
     }
 }
